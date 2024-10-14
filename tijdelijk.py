90c80fa72bdd90cf86ba29a97b436d615f084309
Sjors <sjors.vandenbroek@dynapps.nl>
2024-10-14 19:47:23 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,37 +1,27 @@
-prijzen = {
-    "aardbei": 3,
-    "vanille": 4,
-    "chocolade": 5
-}
-aanbieding = prijzen["aardbei"] * 0.8
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
+from helper import decoreer
 
-#Opdracht 4
-#print(reclame_tekst)
+def print_aanbieding():
+    prijzen = {
+        "aardbei": 3,
+        "vanille": 4,
+        "chocolade": 5
+    }
 
-#Opdracht 5
-# --> Berekenen totaal aantal tekens
-#print(len(reclame_tekst))
+    aanbieding = prijzen["aardbei"] * 0.8
 
-reclame_tekst2 = reclame_tekst[:63]
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
 
-#Opdracht 5
-#print(reclame_tekst2)
+    reclame_tekst2 = reclame_tekst[:63]
 
-reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst3 = reclame_tekst2.upper()
 
-#Opdracht 6
-#print(reclame_tekst3)
+    reclame_tekst4 = reclame_tekst3.split()
 
-reclame_tekst4 = reclame_tekst3.split()
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
 
-#Opdracht 7
-#print(reclame_tekst4)
-
-for el in reclame_tekst4:
-    #Opdracht 9
-    #print(el.lower())
-    if len(el) > 4:
-        print(el.upper())
-    else:
-        print(el.lower())
+decoreer("Aanbieding")
+print_aanbieding()
